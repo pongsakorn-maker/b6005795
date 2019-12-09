13fad61281ac7357c381ae40547dd965c7b64faf
pongsakorn-maker <pongsakorn.reo@gmail.com>
2019-12-09 21:47:26 +0700

    remove team21

--- team21/backend/src/main/java/se/g21/backend/employeesystem/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package se.g21.backend.employeesystem.repository;
-
-import se.g21.backend.employeesystem.entities.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
